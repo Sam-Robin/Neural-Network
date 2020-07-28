20ec74ad4873399a22cceb4d224cb218d94dcbe3
Sam-Robin <samrobinsonwork@outlook.com>
2020-07-28 19:15:03 +0100

    Added y value range tests for the sigmoid class.

--- tests/com/company/neuralnetwork/NeuronTest.java
+++ tests/com/company/neuralnetwork/NeuronTest.java
@@ -19,17 +19,68 @@ class NeuronTest {
     }
 
     @Test
-    void sigmoidUpperRangeIntegers() {
-
+    void sigmoidUpperRangeIntegers()
+    {
         for (double x = 1; x < 1000; x++)
         {
             //Supply sigmoid function with value of x
             double y = neuron.sigmoid(x);
+            //System.out.println("x: " + x + " y: " + y);
 
             //Test that the y value is > 0.5
             assertTrue( y > 0.5,
                     ("Upper range integers fail\t" +
                             "x: " + x + " y: " + y));
+        }
+    }
+
+    @Test
+    void sigmoidUpperRangeRationals()
+    {
+        for (double x = 0.0000001; x < 10; x = x * Math.sqrt(2))    //Chose sqrt(2) because I like it
+        {
+            //Supply sigmoid with value of x
+            double y = neuron.sigmoid(x);
+            //System.out.println("x: " + x + " y: " + y);
+
+            //Test that the y value is > 0.5
+            assertTrue(y > 0.5,
+                    "Upper range rationals fail\t" +
+                            "x: " + x + " y: " + y);
+
+        }
+    }
+
+    @Test
+    void sigmoidLowerRangeIntegers()
+    {
+        for (double x = -1; x > -1000; x--)
+        {
+            //Supply sigmoid function with value of x
+            double y = neuron.sigmoid(x);
+            //System.out.println("x: " + x + " y: " + y);
+
+            //Test that the y value is < 0.5
+            assertTrue( y < 0.5,
+                    ("Lower range integers fail\t" +
+                            "x: " + x + " y: " + y));
+        }
+    }
+
+    @Test
+    void sigmoidLowerRangeRationals()
+    {
+        for (double x = -0.0000001; x > -10; x = x * Math.sqrt(2))    //Chose sqrt(2) because I like it
+        {
+            //Supply sigmoid with value of x
+            double y = neuron.sigmoid(x);
+            System.out.println("x: " + x + " y: " + y);
+
+            //Test that the y value is > 0.5
+            assertTrue(y < 0.5,
+                    "Lower range rationals fail\t" +
+                            "x: " + x + " y: " + y);
+
         }
     }
 }
